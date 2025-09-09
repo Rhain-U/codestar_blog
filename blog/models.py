b8a723d736229eb86e123b92c2a98c2beda8f27b
Rhain Urmston <rhainu@hotmail.co.uk>
2025-09-09 20:18:03 +0100

    Create comments model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Comments(models.Model):
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE,
+related_name = "comments")
+    author = models.ForeignKey(
+        User, on_delete = models.CASCADE,
+related_name = "commenter")
+    body = models.TextField()
+    approved = models.Boolean(default = False)
+    created_on = models.DateTimeField(auto_now_add = True)
+         
